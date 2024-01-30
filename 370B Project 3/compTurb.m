4d6be21c141a6234808271cd36a4557ae2c598db
SreyaVangara <svangara@gmail.com>
2024-01-30 00:07:34 -0800

    updated compturb

--- 370B Project 3/compTurb.m
+++ 370B Project 3/compTurb.m
@@ -1,8 +1,10 @@
 function [Pret,Tret,finalState,pathStates] = compTurb(fluid,P1,T1,P2,eta_p,nsteps)
-% generalized function for a general gas phase compressor or turbine
+% generalized function for a general fluid compressor or turbine
 % performing polytropic compression/expansion
 % Pret is the process path pressure that the compressor/turbine takes
 % Tret is the process path temperature that the compressor/turbine takes
+%
+% [Pret,Tret,finalState,pathStates] = compTurb(fluid,P1,T1,P2,eta_p,nsteps)
 
 % find out the pressure step size
 dP = (P2 - P1)/nsteps;
@@ -12,6 +14,8 @@ hret = 0*Pret;
 sret = 0*Pret;
 vF = 0*Pret; 
 
+% maybe don't do this if the fluid is supposed to be supplied at its
+% initial condition directly into the function
 try
     set(fluid,'T',T1,'P',P1);
 catch % if the fluid is in its saturated state, set using its vapor fraction
@@ -28,7 +32,11 @@ for iter = 1:1:length(Pret)
     set(fluid,'S',s1,'P',Pret(iter));
     h2_s = enthalpy_mass(fluid);
     % use the definition of polytropic efficiency to get the actual h2
-    h2 = h1 + (h2_s-h1)/eta_p;
+    if P2>=P1 % if compressing
+        h2 = h1 + (h2_s-h1)/eta_p;
+    else % if expanding
+        h2 = h1 + (h2_s-h1)*eta_p;
+    end
     set(fluid,'H',h2,'P',Pret(iter));
     % extract all required parameters
     Tret(iter) = temperature(fluid);
@@ -40,7 +48,8 @@ for iter = 1:1:length(Pret)
     catch
         vF(iter) = nan;
     end
-
+    h1 = enthalpy_mass(fluid);
+    s1 = entropy_mass(fluid);
 end
 
 finalState.T = Tret(end);
