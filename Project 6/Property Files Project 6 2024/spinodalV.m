44e2d45aa75703c59082ccf58476c3b564c41240
SreyaVangara <svangara@gmail.com>
2024-02-14 14:49:02 -0800

    fixed 2a

--- Project 6/Property Files Project 6 2024/spinodalV.m
+++ Project 6/Property Files Project 6 2024/spinodalV.m
@@ -3,24 +3,24 @@ function right_spin = Vapor_Spinodal_iT(temp)
     rmin = 0.1;
     dr = 0.001;
 
-    cutoff = 0.001;
+    cutoff = 0.0001;
 
     % Preallocate storage...
-    Prisotherm  = zeros((rmax-rmin)/dr);
+    dPrisotherm  = zeros((rmax-rmin)/dr);
     risotherm   = zeros((rmax-rmin)/dr);
     T = temp;
     i = 1;
     for r=rmin:dr:rmax+dr
         r;
-        Prisotherm(i) = P_irT(ispecies,r,T);
+        dPrisotherm(i) = dPdr_irT(ispecies,r,T);
         risotherm(i) = r;
         i = i+1;
     end
     
     right_spin = 0;
 
-    for j = length(Prisotherm)-1:-1:1
-        if abs(Prisotherm(j+1)-Prisotherm(j)) <=cutoff
+    for j = length(dPrisotherm)-1:-1:1
+        if abs(dPrisotherm(j)) <=cutoff
             right_spin = risotherm(j);
             break
         end
