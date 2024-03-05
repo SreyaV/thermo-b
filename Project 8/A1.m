0d04795e98f303eaa52d30f0e80775eceb32294b
victor deroo <vderoo@stanford.edu>
2024-03-05 12:35:18 -0800

    Part 2 - tray-upward not finished

--- Project 8/A1.m
+++ Project 8/A1.m
@@ -1,11 +1,16 @@
-%% Setup
+%% Setup Reboiler
+
 % Output reboiler- temperature, composition, density (liquid, vapour), complementary outlet phase, feed-specific heat transfer required 
 clc;clear all;close all;
 global N2 O2 Ar N P
+
 N=3;
 Setup_Air_Props;
 P=100000;
+
+% Set Ternary Air
 x_out([N2, O2, Ar])=[0.025, 0.95, 0.025];
+
 i=1;
 for quality_out=0:0.1:1
     [T_in, x_in, y_in, rho_f_in, rho_g_in, T_out, y_out, rho_f_out, rho_g_out, Q_req]=reboiler_bwd(x_out, quality_out);
@@ -16,5 +21,50 @@ for quality_out=0:0.1:1
     quality_data(i)=quality_out;
     i=i+1;
 end
+
+%% Upward tray
+
+i = 9;
+for quality_out = 0.8%:0.1:1
+    i
+    x_in = x_data(i,:);
+    y_in = y_data(i,:);
+    T_vap = 89.5;
+    T_liq = T_data(i,1);
+    quality = quality_data(i);
+    [x_out,y_out,T_liq_out,T_vap_out] = tray_upward(x_in,y_in,T_liq,T_vap,quality);
+    x_top(i,:) = x_out;
+    y_top(i,:) = y_out;
+    T_liq_sat(i,:) = T_liq_out;
+    T_vap_sat(i,:) = T_vap_out;
+    Quality(i) = 0;
+    i = i+1;
+
+end
+
 %%
-plotA1();
+
+clf
+figure(1)
+hold on 
+color = ['g' 'b' 'r'];
+for k=1:3
+    if k==3
+        plot(quality_data,10*x_top(:,k),color = color(k))
+        plot(quality_data,10*y_top(:,k),'--',color = color(k) )
+    else
+        plot(quality_data,x_top(:,k),color = color(k))
+        plot(quality_data,y_top(:,k),'--',color = color(k) )
+    end
+
+end
+hold off
+
+%%
+figure(2)
+plot(quality_data(1:end-3),T_liq_sat)
+hold on
+plot(quality_data(1:end-3),T_vap_sat)
+plotfixer
+
+
---
+++ Project 8/tray_upward.m
@@ -0,0 +1,76 @@
+function [x_out,y_out,T_liq_out,T_vap_out] = tray_upward(x_in,y_in,T_liq,T_vap,quality)
+% Ideal Tray (upwards solving)
+% Input : Vapour composition from below x_in, Liquid composition emerging
+% from the tray y_in and associated Temperatures
+% Output : Vapour emerging from the tray x_out, Liquid composition from
+% above and temperatures
+
+global P
+
+if T_liq>T_vap
+
+    disp('Error in solving the tray upwards')
+
+else 
+    
+    % Find Temperature and Composition of vapour phase (for an ideal tray)
+    [T_vap_out rf rg y_out] = Fast_Bubble_cP(x_in,P);
+    
+    % Bottom quantities are known (quality is in mass here)
+    n_bot_vap = y_in*quality    /M_c(y_in); %moles
+    n_bot_liq = x_in*(1-quality)/M_c(x_in); %moles
+
+    % Iterate on mass flow rate of vapor and check using species continuity
+    % and energy conservation
+    % Method used : Bisection
+
+    m_inf = 0;
+    m_up  = 100;
+    
+    delta = 5e5;
+    while delta > 1e3
+        
+        m = (m_inf + m_up)/2; % Flow rate vapor
+        N = m/M_c(y_out) % Molar flow rate 
+
+        % Species continuity (in moles)
+        n_top_liq = n_bot_liq + N*y_out -n_bot_vap; %moles
+
+        if n_top_liq > 0
+        
+            % Find temperature
+            N_top = sum(n_top_liq);
+            x_out = n_top_liq/N_top
+            [T_liq_out rf rg y] = Fast_Bubble_cP(x_out,P);
+            T_liq_out
+
+            % Energy conservation (in mass)
+            h_bot_liq =  (1-quality)          * h_crT(x_in, rl_cTP(x_in,T_liq,P), T_liq);
+            h_bot_vap =  quality              * h_crT(y_in, rv_cTP(y_in,T_vap,P), T_vap);
+            h_top_liq =  M_c(x_out) * N_top   * h_crT(x_out, rl_cTP(x_out,T_liq_out,P), T_liq_out);
+            h_top_vap =  M_c(y_out) * N       * h_crT(y_out, rv_cTP(y_out,T_vap_out,P), T_vap_out);
+
+            delta = h_top_liq + h_bot_vap - h_bot_liq -h_top_vap
+
+            if delta>0
+            m_inf = m;
+            else
+            m_up = m;      
+            end
+       
+        else
+
+            m_inf = m;
+        
+        end
+
+
+
+
+
+        delta = abs(delta);
+
+    end
+
+
+end
