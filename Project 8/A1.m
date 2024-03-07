459f232b4160b37ee97119abf3e83a90f0e750e1
SreyaVangara <svangara@gmail.com>
2024-03-06 18:28:47 -0800

    fixed condenser step and checked with ta

--- Project 8/A1.m
+++ Project 8/A1.m
@@ -48,6 +48,29 @@ end
 
 %% Condenser 
 
+% Output reboiler- temperature, composition, density (liquid, vapour), complementary outlet phase, feed-specific heat transfer required 
+clc;clear all;close all;
+global N2 O2 Ar N P
+
+N=3;
+Setup_Air_Props;
+P=100000;
+
+% Set Ternary Air
+y_feed([N2, O2, Ar])=[0.025, 0.95, 0.025];
+
+i=1;
+for quality_out=0:0.1:1
+    [T_in, x_in, y_in, rho_f_in, rho_g_in, T_out, y_out, rho_f_out, rho_g_out, Q_req]=condenser_bwd(y_feed, quality_out);
+    rho_g_out(i) = rho_g_out;
+    x_data(i, :)=x_in;
+    y_data(i, :)=y_out;
+    T_data(i, 1:2)=[T_in, T_out];
+    Q_data(i)=Q_req;
+    quality_data(i)=quality_out;
+    i=i+1;
+end
+
 %% Downward tray
 
 i = 1;
