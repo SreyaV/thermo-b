0389c551f712697a480271a8829d37f724ba7fc9
victor deroo <vderoo@stanford.edu>
2024-03-06 22:12:43 -0800

    downwards tray

--- Project 8/tray_downward.m
+++ Project 8/tray_downward.m
@@ -30,7 +30,7 @@ else
     delta = 5e5;
     while delta > 1e3
         
-        m = (m_inf + m_up)/2 % Flow rate liquid (downwards)
+        m = (m_inf + m_up)/2; % Flow rate liquid (downwards)
         N = m/M_c(x_out); % Molar flow rate 
 
         % Species continuity (in moles)
@@ -42,8 +42,7 @@ else
             N_bot = sum(n_bot_vap);
             y_out = n_bot_vap/sum(n_bot_vap);
             [T_vap_out rg_out rf_in y] = Fast_Dew_cP(y_out,P);
-            T_vap_out
-            mass_check = M_c(x_out)*N_bot + m_vap - m_liq -m
+            mass_check = M_c(x_out)*N_bot + m_vap - m_liq -m;
 
             % Energy conservation (in mass)
             h_bot_liq =  m                 * h_crT(x_out , rf_out, T_liq_out);
@@ -52,7 +51,7 @@ else
             h_top_vap =  m_vap             * h_crT(y_out, rg_in, T_vap);
 
       
-            delta = h_top_liq + h_bot_vap - h_bot_liq -h_top_vap
+            delta = h_top_liq + h_bot_vap - h_bot_liq -h_top_vap;
 
             if delta < 0
                 m_inf = m;
