a93ffd9c78f1c3a2647924ffc951f578dfbf23f9
SreyaVangara <svangara@gmail.com>
2024-03-11 18:55:52 -0700

    Pset 9 start

---
+++ Project 9/Part1.m
@@ -0,0 +1,25 @@
+%Button Cell
+
+%Givens
+
+temp = 1000+273;
+P = 100000;
+cond_YSZ = 15; % S/m
+D_H2H2O = 3.8378e-3;
+D_O2N2 = 2.9417e-4;
+
+t_e = 50e-6; 
+t_GDL = 5e-3;
+
+N_H2 = 0.97;
+N_H2O = 0.03;
+
+J_cathode = 1000; %A/m^2
+J_anode = 100*J_cathode;
+
+
+%% Anode
+
+
+
+%% 
